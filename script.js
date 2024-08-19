31e120c6e64b99e8d95cf3a3a8648e577847e765
Alaa Saad <alaa1.saed71@gmail.com>
2024-08-19 09:48:05 +0300

    Calendar

---
+++ script.js
@@ -0,0 +1,14 @@
+const month= document.getElementById("month");
+const day= document.getElementById("day");
+const dayNum= document.getElementById("day-number");
+const year= document.getElementById("year");
+const date=new Date();
+const monthEl=date.getMonth();
+year.innerHTML=date.getFullYear();
+dayNum.innerHTML=date.getDate();
+day.innerHTML=date.toLocaleDateString("en",{
+    weekday:"long",
+})
+month.innerHTML=date.toLocaleDateString("en",{
+    month:"long",
+})
